3b5254f66ef6215ab3b04d116c1176137edad988
Brian <hibrian827@gmail.com>
2024-12-14 16:24:30 +0900

    testing

---
+++ Test/16_buffered.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+  printf("h");
+  printf("e");
+  printf("l");
+  printf("l");
+  printf("o");
+  printf("\n");
+  fflush(stdout);
+}
---
+++ Test/16_bufferedFile.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+  char buf[1024];
+  FILE *infile = stdin;
+  if (argc == 2) {
+    infile = fopen(argv[1], "r");
+  }
+  while(fgets(buf, 1024, infile) != NULL) {
+    printf("----\n");
+    fprintf(stdout, buf);
+  }
+}
